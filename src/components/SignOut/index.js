919ab6d47e69f4a954d0eb80f20c321829f921f3
Nathan Wang <nathan.wang2@gmail.com>
2019-08-23 07:31:52 -0500

    Added ability to reset passwords

--- src/components/SignOut/index.js
+++ src/components/SignOut/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { withFirebase } from "../Firebase";
 
+// TODO This should reroute to the landing page
 const SignOut = ({ firebase }) => (
   <button type="button" onClick={firebase.SignOut}>
     Sign Out
