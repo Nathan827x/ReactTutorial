e26ce292d6153ac439d33bfaae1804863def903b
Nathan <nathan.wang2@gmail.com>
2019-08-24 01:38:44 -0500

    Blocked home page and account page based on if user is logged in

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -1,12 +1,19 @@
 import React from "react";
 
 import PasswordChangeForm from "../PasswordChange";
+import { withAuthorization, AuthUserContext } from "../Session";
 
 const AccountPage = () => (
-  <div>
-    <h1>Account Page</h1>
-    <PasswordChangeForm />
-  </div>
+  <AuthUserContext.Consumer>
+    {authUser => (
+      <div>
+        <h1>Account Page</h1>
+        <PasswordChangeForm />
+      </div>
+    )}
+  </AuthUserContext.Consumer>
 );
 
-export default AccountPage;
+const condition = authUser => !!authUser;
+
+export default withAuthorization(condition)(AccountPage);
